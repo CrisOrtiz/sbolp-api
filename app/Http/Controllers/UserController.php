443da81fc898a05c64b235a9e39619c4dee86156
CrisOrtiz <cristhianortizmercado@gmail.com>
2020-11-20 12:15:06 -0400

    modified user controller index

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,11 +12,20 @@ class UserController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index($pageSize, $currentPage, $search, $orderBy)
     {
-        $users = User::all();
+        $users = User::where('name','LIKE','%'.$search.'%')
+            ->orWhere('lastname','LIKE','%'.$search.'%')
+            ->orWhere('email','LIKE','%'.$search.'%')
+            ->orderBy($orderBy)
+            ->paginate((int)$pageSize)
+            ->skip((int)$currentPage * (int)$pageSize)
+            ->get();
+
+        $totalItem = $users->count();
+        $totalPage = $currentPage;
        
-        return $users;
+        return response()->json(compact(['users','totalItem', 'totalPage']));
     }
 
 }
