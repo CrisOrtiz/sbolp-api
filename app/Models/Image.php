65fe1cc004c9cf4dd5abd1aa7059159281bbf21b
Cristhian Ortiz Mercado <Cristhian.Ortiz@fundacion-jala.org>
2022-05-18 15:41:25 -0400

    changed models connection

--- app/Models/Image.php
+++ app/Models/Image.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Image extends Model
 {
-    protected $connection = 'mysql';
+    protected $connection = 'pgsql';
 
     /**
      * The table associated with the model.
