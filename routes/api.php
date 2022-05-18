ccf7f3676dd2b071b93ccba873609f41f54ccf40
Cristhian Ortiz Mercado <Cristhian.Ortiz@fundacion-jala.org>
2022-05-18 01:35:31 -0400

    remove use Router

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use Dingo\Api\Routing\Router;
 
 /*
 |--------------------------------------------------------------------------
