003c9f9a6accf70de7235c9931ee65a53b21c8bf
Cristhian Ortiz Mercado <Cristhian.Ortiz@fundacion-jala.org>
2022-05-18 14:10:47 -0400

    add dingo test routes

--- routes/api.php
+++ routes/api.php
@@ -73,7 +73,12 @@ Route::group(['prefix' => '', 'middleware' => 'auth:api'], function () {
 
 $api = app('Dingo\Api\Routing\Router');
 $api->version('v1', ['middleware' => 'api.throttle', 'limit' => 100, 'expires' => 5], function ($api) {
-    $api->post('test2', 'AuthController@test');
+    $api->group(['namespace' => 'App\Http\Controllers',], function ($api) {
+        $api->post('login2', 'AuthController@login');
+        $api->post('register2', 'AuthController@register');
+
+        $api->post('test2', 'AuthController@test');
+    });
 });
 
 /*$api->version('v1', ['middleware' => 'api.throttle', 'limit' => 100, 'expires' => 5], function ($api) {
