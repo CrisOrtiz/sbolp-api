f7ed052761f9db9b2c17195c8b42f6e75e699623
Cristhian Ortiz Mercado <Cristhian.Ortiz@fundacion-jala.org>
2022-05-27 18:57:40 -0400

    restore migrations to default mysql driver

--- database/migrations/2020_10_27_211031_user_cases.php
+++ database/migrations/2020_10_27_211031_user_cases.php
@@ -13,7 +13,7 @@ class UserCases extends Migration
      */
     public function up()
     {
-        Schema::connection('pgsql')->create('user_cases', function (Blueprint $table) {
+        Schema::connection('mysql')->create('user_cases', function (Blueprint $table) {
             $table->id();
             $table->string('user_id');
             $table->string('case_id');
