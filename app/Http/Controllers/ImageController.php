32598e2d91010487de3c1ebcf5a0b40c09830962
Cristhian Ortiz Mercado <Cristhian.Ortiz@fundacion-jala.org>
2022-09-16 04:27:40 -0400

    deleting images working

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers;
 
-use File;
+use Illuminate\Support\Facades\File;
 use App\Models\User;
 use App\Models\ClinicCase;
 use App\Models\Comment;
@@ -18,7 +18,7 @@ class ImageController extends Controller
     public function store(Request $request)
     {
         if ($request->hasFile('file')) {
-            $avoidOverride = true;
+
             $image = $request->file('file');
             if ($image->getSize() > 5000000) {
                 $message = 'Imagen muy grande, max 5Mb.';
@@ -36,8 +36,6 @@ class ImageController extends Controller
             } elseif ($request->rel_type == 'profile') {
                 if (!File::isDirectory(public_path('/img/users/') . $request->id . '/')) {
                     File::makeDirectory(public_path('/img/users/') . $request->id . '/', 0777, true, true);
-                } else {
-                    $avoidOverride = false;
                 }
                 $item = User::findOrFail($request->id);
                 $destinationPath = public_path('/img/users/' . $request->id . '/');
@@ -58,7 +56,9 @@ class ImageController extends Controller
                     $constraint->aspectRatio();
                 })->save($destinationPath . $name);
 
-            if ($avoidOverride) {
+            $imageExists = Image::where('image_name', $name)->first();
+
+            if (!$imageExists) {
                 $imageRecord = new Image();
                 $imageRecord->rel_type = $request->rel_type;
                 $imageRecord->rel_id = $request->id;
@@ -67,7 +67,7 @@ class ImageController extends Controller
                 $imageRecord->image_name = $name;
                 $imageRecord->save();
             } else {
-                $imageRecord = Image::where('image_name', $request->image_name)->first();
+                $imageRecord = $imageExists;
             }
 
             $message = 'Image uploaded successfully';
@@ -80,25 +80,33 @@ class ImageController extends Controller
 
     public function delete(Request $request)
     {
-
-        if ($request->rel_type == 'article') {
-            $item = Article::findOrFail($request->id);
-            $image_name = $item->image_name;
-            $filePath = public_path('/img/posts/' . $image_name);
+        $message = 'invalid rel type';
+        $imageUrl = $request->img_url;
+        if ($request->rel_type == 'case') {
+            if (File::exists(public_path($imageUrl))) {
+                if (Image::where('image_url', $imageUrl)->delete()) {
+                    File::delete(public_path($imageUrl));
+                } else {
+                    $message = 'error on delete file';
+                    return response()->json(compact('message'), 400);
+                }
+            }
         } elseif ($request->rel_type == 'user') {
-            $item = User::findOrFail($request->id);
-            $image_name = $item->image_name;
-            $filePath = public_path('/img/users/' . $image_name);
+            $id = $request->user_id;
+            $user = User::findOrFail($id);
+            if (File::exists(public_path($imageUrl))) {
+                if (Image::where('image_url', $imageUrl)->delete()) {
+                    File::delete(public_path($imageUrl));
+                } else {
+                    $message = 'error on delete file';
+                    return response()->json(compact('message'), 400);
+                }
+                $user->image_url = 'img/users/default-user.jpg';
+                $user->save();
+            }
         } else {
             $message = 'invalid rel type';
             return response()->json(compact('message'), 400);
-        }
-
-        if (File::exists($filePath)) {
-            File::delete($filePath);
-            $message = 'Image removed successfully';
-            $item->image_name = null;
-            $item->save();
         }
 
         return response()->json(compact('message'), 200);
